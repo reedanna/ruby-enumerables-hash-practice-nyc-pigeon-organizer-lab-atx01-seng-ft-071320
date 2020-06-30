9e84bf512c3612b397283947262bec52a11a1e4a
Anna Nicole Reed <reedanna@mail.com>
2020-06-30 02:00:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,44 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # this is where the output hash will go
+  pigeon_list = {}
+  
+  #this is for saving values between enumerables
+  category = ""
+  attribute = ""
+  
+  data.each do |key, value|
+    #this is for the top layer of hash--keys are attributes, values are colors/genders/places
+    value.each do |key, value|
+      #second layer of hash--keys are colors/genders/places, values are arrays of pigeon names
+      value.each do |item|
+        #these are arrays of pigeon names
+        if pigeon_list.any?(item) == false
+          #checks to see if the name is in the pigeon list already, and if not...
+          pigeon_list[item] = {
+            :color => [],
+            :gender => [],
+            :lives => []
+          }
+          #adds it
+        end
+      end
+    end
+  end
+  
+  data.each do |key, value|
+    #this is for the top layer of hash--keys are attributes, values are colors/genders/places
+    category = key
+    value.each do |key, value|
+      #second layer of hash--keys are colors/genders/places, values are arrays of pigeon names
+      attribute = key
+      value.each do |item|
+        #these are arrays of pigeon names
+        pigeon_list[item][category].push(attribute.to_s)
+      end
+    end
+  end
+    
+  
+  #return the pigeon list
+  pigeon_list
 end
