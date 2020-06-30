09b84daeb6f9e8dc424f319e2268efbb10f29ba7
Anna Nicole Reed <reedanna@mail.com>
2020-06-30 01:53:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,53 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # this is where the output hash will go
+  pigeon_list = {}
+  
+  #this is for saving values between enumerables
+  category = ""
+  attribute = ""
+  
+  data.each do |key, value|
+    #this is for the top layer of hash--keys are attributes, values are colors/genders/places
+    category = key
+    value.each do |key, value|
+      #second layer of hash--keys are colors/genders/places, values are arrays of pigeon names
+      attribute = key
+      value.each do |item|
+        #these are arrays of pigeon names
+        if pigeon_list.any?(item) == false
+          #checks to see if the name is in the pigeon list already, and if not...
+          pigeon_list[item] = {
+            :color => [],
+            :gender => [],
+            :lives => []
+          }
+          #adds it
+        end
+        pigeon_list[item][category].push(attribute)
+      end
+    end
+  end
+  
+  #return the pigeon list
+  print pigeon_list
 end
+
+#nyc_pigeon_organizer(
+  pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+#})
